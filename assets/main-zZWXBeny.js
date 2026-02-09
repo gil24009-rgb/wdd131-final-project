3ae78d617da9beb3d077db7100bac61654cc64c6
gil24009-rgb <gil24009-rgb@users.noreply.github.com>
2026-02-09 22:01:56 +0000

    deploy: ba806e7590e125eb7f5cb2401524ff0fffe7f358

--- assets/main-zZWXBeny.js
+++ assets/main-zZWXBeny.js
@@ -1,2 +1,2 @@
-import{g as o,a,s as i,m as r}from"./setHeaderFooter-eGNdt7RF.js";function s(n){const t=document.querySelector(".intro");t.innerHTML=`<h1>${n.fullName}</h1>
+import{g as o,a,s as i,m as r}from"./setHeaderFooter-DWgclDoh.js";function s(n){const t=document.querySelector(".intro");t.innerHTML=`<h1>${n.fullName}</h1>
   <p>${n.description}</p>`}function c(n){const t=document.querySelector(".info"),e=n.map(r);t.insertAdjacentHTML("afterbegin",e.join(""))}async function m(){const n=await o(),t=a(n.images);i(n),s(n),c(t)}m();
